03c4b87af0326e58f1aa2eea1cab4ca90391abf2
Bruno Pereira <brunopereira@acad.ifma.edu.br>
2024-11-25 21:12:58 -0300

    Update index.js

--- PrimeiraEtapa/index.js
+++ PrimeiraEtapa/index.js
@@ -1,7 +1,7 @@
 import cesar from './cesar.js'
 
-let mensagemCrip = cesar.criptografia("zyx", 3, cesar.cifrarLetras)
-let mensagemDes = cesar.criptografia("cba", 3, cesar.decifrarLetras)
+let mensagemCrip = cesar.criptografia("brunopereira", 3, cesar.cifrarLetras)
+let mensagemDes = cesar.criptografia("euxqrshuhlud", 3, cesar.decifrarLetras)
 
-console.log(mensagemCrip)
-console.log(mensagemDes)
+console.log("Mensagem criptografada:", mensagemCrip.join(''))
+console.log("Mensagem descriptografada:", mensagemDes.join(''))
